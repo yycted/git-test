53ebe8a21b24d85ca74312f63b29f4da39e4c8dd
yycted <522209839@qq.com>
2021-07-16 23:54:58 +0800

    修改克隆下的firstclass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -10,9 +10,11 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git ");
+        //hello
     }
 
     public void test() {
         int i = 0;
+        System.out.println(i);
     }
 }
