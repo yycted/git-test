d0080bc43c04601eaafafb5b491aac8be4085178
yycted <522209839@qq.com>
2021-07-16 22:57:10 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.itheima.git;
+
+/**
+ * @author : YYC
+ * @Description : git 测试
+ * @Creation Date: 2021-07-16 22:41
+ * @ModificationHistory Who  When  What
+ * -----------------------------------------------------
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git ");
+    }
+}
